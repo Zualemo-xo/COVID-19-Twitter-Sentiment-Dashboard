06d74357800139562968dc90d164d5946de258ac
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2020-07-30 20:58:45 +0530

    Update credentials.py

--- COVID-19 Sentiment Analysis Dashboard/credentials.py
+++ COVID-19 Sentiment Analysis Dashboard/credentials.py
@@ -1,8 +1,8 @@
-#Twitter API keys
+#Twitter API keys(To be filled by the user)
 API_key=''
 API_secret_key=''
 access_token=''
 access_token_secret=''
 
-#Plotly mapbox token
+#Plotly mapbox token(To be filled by the user)
 plotly_token=''
